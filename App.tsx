1defbfee062c2466610491c7ba2d218ca3950857
Alexander R <ru5nedug@mail.ru>
2022-08-25 16:46:50 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,55 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import {
-    ActivityIndicator,
-    Alert,
-    Button,
-    Image,
-    Keyboard,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableWithoutFeedback,
-    View,
-} from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, ListRenderItem } from 'react-native';
+import { useCallback } from 'react';
+
+type arrayDataType = {
+    id: number
+    title: string
+}
+
+const arrayData: arrayDataType[] = new Array(100)
+   .fill(null)
+   .map((_, index) => ( {
+    id: index + 1,
+    title: `Title_${index + 1}`,
+} ))
 
 export default function App() {
 
-    const [value, setValue] = useState('');
+    const renderItem: ListRenderItem<arrayDataType> = useCallback(({ item }) => (
+       <View>
+           <Text>
+               {item.title}
+           </Text>
+       </View>
+    ), []);
 
+    const keyExtractor = useCallback((item, index) => `${item.title}.${index}`, []);
 
     return (
-       <TouchableWithoutFeedback
-          style={{ flex: 1 }}
-          onPress={() => {
-              Keyboard.dismiss()
-          }}
-       >
-           <View style={styles.container}>
-               <View>
-                   <Text>Open up App.tsx to start working on your app!</Text>
-                   <ActivityIndicator />
-                   <ActivityIndicator size="large" />
-                   <ActivityIndicator size="small" color="#0000ff" />
-                   <ActivityIndicator size="large" color="#ff2c28" />
-                   <Button
-                      title="Press me"
-                      onPress={() => Alert.alert('Simple Button pressed')}
-                   />
-                   <Image
-                      style={styles.img}
-                      source={{ uri: 'https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png' }}
-                   />
-                   <TextInput
-                      style={styles.input}
-                      placeholder="type here"
-                      value={value}
-                      onChangeText={setValue}
-                   />
-               </View>
-               <StatusBar style="auto" />
-           </View>
-       </TouchableWithoutFeedback>
+       <View style={styles.container}>
+           <Text>dfbfdbfdb</Text>
+           <FlatList
+              data={arrayData}
+              renderItem={renderItem}
+              keyExtractor={keyExtractor}
+           >
+           </FlatList>
+           <StatusBar style="auto" />
+       </View>
     );
 }
 
@@ -57,17 +44,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    img: {
-        width: 150,
-        height: 150,
-    },
-    input: {
-        width: 300,
-        height: 36,
-        borderWidth: 1,
-        borderRadius: 10,
+        marginTop: 50,
     },
 });
