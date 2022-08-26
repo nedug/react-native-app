d7ae44d7be1d44ae985c2ce96f62288edcb773cf
Alexander R <ru5nedug@mail.ru>
2022-08-26 13:45:15 +0300

    Update Main.tsx

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 
 
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 export const Main = () => {
+
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {
+            id: 1,
+            title: 'HTML',
+            isDone: true,
+        },
+        {
+            id: 2,
+            title: 'CSS',
+            isDone: true,
+        },
+        {
+            id: 3,
+            title: 'React Native',
+            isDone: false,
+        },
+    ]);
+
 
     return (
        <View>
